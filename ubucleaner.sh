17de789bba178697f5e4f3f7a6a367eebf274580
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-06 20:35:38 -0500

    shellcheck

--- ubucleaner.sh
+++ ubucleaner.sh
@@ -10,7 +10,7 @@ OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
 CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
 LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
 METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
-OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
+OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v "$CURKERNEL")
 YELLOW="\033[1;33m"
 RED="\033[0;31m"
 ENDCOLOR="\033[0m"
@@ -22,10 +22,10 @@ if [ $USER != root ]; then
 fi
 
 echo -e $YELLOW"Removing old config files..."$ENDCOLOR
-apt-get purge $OLDCONF
+apt-get purge "$OLDCONF"
 
 echo -e $YELLOW"Removing old kernels..."$ENDCOLOR
-apt-get purge $OLDKERNELS
+apt-get purge "$OLDKERNELS"
 
 echo -e $YELLOW"Cleaning apt cache..."$ENDCOLOR
 apt-get autoclean && apt-get clean
